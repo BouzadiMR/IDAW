6d2d04f274279125f88aa431f8c56f3bc3419f70
« BouzadiMR» <Marouane.bouzadi@etu.imt-nord-europe.fr>
2023-10-10 09:39:11 +0200

    Test 1 PHP

---
+++ TP2-PHP/indexheure.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Afficher Heure</title>
+</head>
+<body>
+    <h1>L'heure courante est:</h1>
+    <?php
+        echo date('H:i:s');  // Affiche l'heure courante au format heures:minutes:secondes
+    ?>
+</body>
+</html>
