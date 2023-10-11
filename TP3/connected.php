3899ae99934cebfd4d5ed13e96ad2b9be38af7a3
« BouzadiMR» <Marouane.bouzadi@etu.imt-nord-europe.fr>
2023-10-11 09:31:44 +0200

    TEST partie 2

--- TP3/connected.php
+++ TP3/connected.php
@@ -1,25 +1,29 @@
 <?php
-// on simule une base de données
 $users = array(
-// login => password
-   'Marouane' => 'Bouzadi',
-   'yoda' => 'maitrejedi' );
-$login = "anonymous";
-$errorText = "";
-$successfullyLogged = false;
-if(isset($_GET['login']) && isset($_GET['password'])) {$tryLogin=$_GET['login'];
-    $tryPwd=$_GET['password'];
-    // si login existe et password correspond
-    if( array_key_exists($tryLogin,$users) && $users[$tryLogin]==$tryPwd ) {
-    $successfullyLogged = true;
-    $login = $tryLogin;
-    } else
-       $errorText = "Erreur de login/password";
-    } else
-       $errorText = "Merci d'utiliser le formulaire de login";
-    if(!$successfullyLogged) {
-         echo $errorText;
+    'Marouane' => 'Bouzadi',
+    'yoda' => 'maitrejedi'
+); 
+
+$login = "anonymous"; 
+$errorText = ""; // Retirez la barre oblique qui était ici.
+$successfullyLogged = false; 
+
+if (isset($_POST['login']) && isset($_POST['password'])) {
+    $tryLogin = $_POST['login'];
+    $tryPwd = $_POST['password'];
+    if (array_key_exists($tryLogin, $users) && $users[$tryLogin] == $tryPwd) {
+        $successfullyLogged = true;
+        $login = $tryLogin;
     } else {
-         echo "<h1>Bienvenu ".$login."</h1>";
+        $errorText = "Erreur de login/password";
     }
-    ?>
+} else {
+    $errorText = "Merci d'utiliser le formulaire de login";
+}
+
+if (!$successfullyLogged) {
+    echo $errorText;
+} else {
+    echo "<h1>Bienvenu " . $login . "</h1>";
+}
+?>
