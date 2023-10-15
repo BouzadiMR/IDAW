dd1b70d1065fda280d0e1fe1d6a4a57dfa785945
« BouzadiMR» <Marouane.bouzadi@etu.imt-nord-europe.fr>
2023-10-15 23:10:04 +0200

    v3

--- SitePro/v3/fr/contact.php
+++ SitePro/v3/fr/contact.php
@@ -2,7 +2,7 @@
 
 <section class="contact-section">
     <h2>Contactez-Moi</h2>
-    <p><strong>Adresse:</strong> 27 Place De Mons, Douai</p>
+    <p><strong>Adresse:</strong> <a href=27 Place De Mons, Douai>27 Place De Mons, Douai</a></p>
     <p><strong>Email:</strong> <a href="mailto:marouan.bouzadi123@gmail.com">marouan.bouzadi123@gmail.com</a></p>
     <p><strong>Téléphone:</strong> <a href="tel:+33763700202">+33 7 63 70 02 02</a></p>
 </section>
